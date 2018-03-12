06ee01956266ee64faec7c93a243fe90d7ec38c4
DESKTOP-2BJDEOE\11723 <172304645@qq.com>
2018-03-12 16:17:10 +0800

    测试

--- src/main/java/com/nix/good/service/BaseService.java
+++ src/main/java/com/nix/good/service/BaseService.java
@@ -1,5 +1,6 @@
 package com.nix.good.service;
 
+import com.nix.good.dao.base.BaseMapper;
 import com.nix.good.model.base.BaseModel;
 import com.nix.good.util.SQLUtil;
 import com.nix.good.util.log.LogKit;
@@ -17,38 +18,18 @@ import java.util.List;
  * 实现了对继承{@link BaseModel}的model基础的增删改查接口
  */
 @Service
-public class BaseService <M extends BaseModel<M>>{
+public abstract class BaseService <M extends BaseModel<M>>{
+    protected BaseMapper<M> baseMapper;
 
+    protected abstract void init(BaseMapper<M> mBaseMapper);
 
-    /**
-     * 反射执行执行dao方法（有时间改为动态代理）
-     * @param methodName 需要执行的dao方法名
-     * @param clazzs 方法参数对象组类类型（为了反射时找到具体方法）
-     * @param objects 调用方法参数组（执行方法需要的参数）
-     * @return 返回方法执行结果
-     * @throws Exception 抛出反射执行过程中出现的异常
-     * */
-    private Object invokeMapperMethod(String methodName,Class[] clazzs,Object ... objects) throws Exception {
-        LogKit.info(this.getClass(),"执行" + methodName + "方法");
+    public void add(M model) throws Exception {
         try {
-            Object o = SpringContextHolder.getBean(this.getClass().getSimpleName().toLowerCase().replaceFirst("service","Mapper"));
-            Class<?> clazz = o.getClass();
-            Method method = clazz.getMethod(methodName, clazzs);
-            return method.invoke(o,objects);
-        }catch (Exception e){
+            baseMapper.insert(model);
+        }catch (Exception e) {
             throw e;
         }
-    }
-    /**
-     * 去dao中找具体的方法
-     * @param methodName 需要找的方法名
-     * @param model 与dao绑定的model类
-     * */
-    private void callInvoke(String methodName,M model) throws Exception {
-        invokeMapperMethod(methodName,new Class[]{model.getClass()},model);
-    }
-    public void add(M model) throws Exception {
-        callInvoke("insert",model);
+
     }
 
     /**
@@ -58,9 +39,14 @@ public class BaseService <M extends BaseModel<M>>{
      * @throws Exception 删除失败抛出异常
      * */
     public void delete(Integer[] ids) throws Exception {
-        for (Integer id:ids) {
-            delete(id);
+        try {
+            for (Integer id:ids) {
+                delete(id);
+            }
+        }catch (Exception e) {
+            throw e;
         }
+
     }
     /**
      * 在数据库中删除一个对象
@@ -69,7 +55,12 @@ public class BaseService <M extends BaseModel<M>>{
      * @throws Exception 删除失败抛出异常
      * */
     public void delete(Integer id) throws Exception {
-        invokeMapperMethod("delete",new Class[]{Integer.class},id);
+        try {
+            baseMapper.delete(id);
+        }catch (Exception e) {
+            throw e;
+        }
+
     }
     /**
      *
@@ -80,7 +71,11 @@ public class BaseService <M extends BaseModel<M>>{
      *
      * */
     public void update(M model) throws Exception {
-        callInvoke("update",model);
+        try {
+            baseMapper.update(model);
+        }catch (Exception e) {
+            throw e;
+        }
     }
 
 
@@ -96,9 +91,8 @@ public class BaseService <M extends BaseModel<M>>{
      * */
     public List<M> list(Integer page,Integer size,String order,String sort,String conditionsSql){
         try {
-            Object find = invokeMapperMethod("list", new Class[]{Integer.class,Integer.class,String.class,String.class,String.class},
-                    SQLUtil.getOffset(page,size), size,order,sort,conditionsSql);
-            return (List<M>) find;
+            List<M> find = baseMapper.list(SQLUtil.getOffset(page,size), size,order,sort,conditionsSql);
+            return find;
         }catch (Exception e){
             e.printStackTrace();
             return null;
@@ -114,8 +108,8 @@ public class BaseService <M extends BaseModel<M>>{
      * */
     public M findById(Integer id){
         try {
-            Object find = invokeMapperMethod("select",new Class[]{Integer.class},id);
-            return (M) find;
+            M find = baseMapper.select(id);
+            return find;
         }catch (Exception e){
             e.printStackTrace();
             return null;
