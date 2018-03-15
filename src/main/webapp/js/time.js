a40e72608b47b628426e0bf93d9db5f503a57fbb
DESKTOP-2BJDEOE\11723 <172304645@qq.com>
2018-03-15 21:22:10 +0800

    添加时间插件

---
+++ src/main/webapp/js/time.js
@@ -0,0 +1,12 @@
+$(function () {
+    //日期插件
+    $(".input_time").datetimepicker({
+        format: 'yyyy-mm-dd',//显示格式
+        todayHighlight: 1,//今天高亮
+        minView: "month",//设置只显示到月份
+        startView:2,
+        forceParse: 0,
+        showMeridian: 1,
+        autoclose: 1//选择后自动关闭
+    });
+});
